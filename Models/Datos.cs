6e363648b6e05cc77c62460f8e02e2695af2a1bb
Roberto1Rodriguez <robertordzw@gmail.com>
2022-10-02 18:34:24 -0500

    Datos

--- Models/Datos.cs
+++ Models/Datos.cs
@@ -8,6 +8,11 @@ namespace DibujoTCP.Models
 {
     public class Datos
     {
+        public int Alto { get; set; }
+        public int Ancho { get; set; }
+        public int CoordenadaY { get; set; }
+        public int CoordenadaX { get; set; }
+        public string color { get; set; } = "";
 
     }
 }
