36acdd516e5c23a5ee470df779201370e5644326
Roberto1Rodriguez <robertordzw@gmail.com>
2022-10-02 19:22:17 -0500

    Intento1

---
+++ ViewModels/DrawViewModel.cs
@@ -0,0 +1,35 @@
+﻿using DibujoTCP.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DibujoTCP.ViewModels
+{
+    public class DrawViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Datos> Dibujos = new ObservableCollection<Datos>();
+        public string Error { get; set; } = "";
+        private Datos datos;
+        public Datos Datos
+        {
+            get { return datos; }
+            set { datos = value; PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("Datos")); ; }
+        }
+        
+        public void Rectangulo()
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = datos.Ancho;
+            rect.Height = datos.Alto;
+            rect.X = datos.CoordenadaX;
+            rect.Y = datos.CoordenadaY;
+
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
